76fe4d9016f4bffd0961e09f26f279081f6ff295
Ronney Arias <ronneyarias@gmail.com>
2024-05-24 10:26:31 -0500

    complete lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,12 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", mode='w') as f:
+        f.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", mode='a') as f:
+        f.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", encoding="utf-8") as f:
+        open_content = f.read()
+    return open_content
